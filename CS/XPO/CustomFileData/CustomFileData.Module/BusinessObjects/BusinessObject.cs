25f95d474eb099e8ab5849ecba90b8dbaa6575d5
andrey <andrey.kozhevnikov@devexpress.com>
2023-10-16 19:02:22 +0400

    init xpo

---
+++ CS/XPO/CustomFileData/CustomFileData.Module/BusinessObjects/BusinessObject.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFileData.Module.BusinessObjects {
+
+    [DefaultClassOptions]
+    public partial class BusinessObject : XPLiteObject {
+        public BusinessObject(Session session) : base(session) {
+            _DocumentFile = new InplaceFileData(this, PropertyNameEnum.Document);
+            _ScreenshotFile = new InplaceFileData(this, PropertyNameEnum.Screenshot);
+        }
+
+        int fID;
+        [Key(true)]
+        public int ID {
+            get { return fID; }
+            set { SetPropertyValue<int>(nameof(ID), ref fID, value); }
+        }
+        string fDescription;
+        public string Description {
+            get { return fDescription; }
+            set { SetPropertyValue<string>(nameof(Description), ref fDescription, value); }
+        }
+        byte[] fDocument;
+        [Size(SizeAttribute.Unlimited)]
+        [MemberDesignTimeVisibility(true)]
+        [Browsable(false)]
+        public byte[] Document {
+            get { return fDocument; }
+            set { SetPropertyValue<byte[]>(nameof(Document), ref fDocument, value); }
+        }
+        byte[] fScreenshot;
+        [Size(SizeAttribute.Unlimited)]
+        [MemberDesignTimeVisibility(true)]
+        [Browsable(false)]
+        public byte[] Screenshot {
+            get { return fScreenshot; }
+            set { SetPropertyValue<byte[]>(nameof(Screenshot), ref fScreenshot, value); }
+        }
+
+        private InplaceFileData _DocumentFile;
+        public InplaceFileData DocumentFile { get { return _DocumentFile; }set { } }
+        private InplaceFileData _ScreenshotFile;
+        public InplaceFileData ScreenshotFile { get { return _ScreenshotFile; } set { } }
+    }
+    public enum PropertyNameEnum { Document, Screenshot }
+}
---
+++ CS/XPO/CustomFileData/CustomFileData.Module/BusinessObjects/InplaceFileData.cs
@@ -0,0 +1,93 @@
+﻿using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using System.ComponentModel;
+
+namespace CustomFileData.Module.BusinessObjects {
+    [DomainComponent]
+    public class InplaceFileData : IFileData {
+        private BusinessObject owner;
+        private PropertyNameEnum propertyName;
+        public InplaceFileData(BusinessObject _owner, PropertyNameEnum _propertyName) {
+            this.owner = _owner;
+            this.propertyName = _propertyName;
+        }
+        public void Clear() {
+            switch (propertyName) {
+                case PropertyNameEnum.Document:
+                    owner.Document = null;
+                    break;
+                case PropertyNameEnum.Screenshot:
+                    owner.Screenshot = null;
+                    break;
+            }
+        }
+        public string FileName {
+            get {
+                string ext = null;
+                switch (propertyName) {
+                    case PropertyNameEnum.Document:
+                        ext = "txt";
+                        break;
+                    case PropertyNameEnum.Screenshot:
+                        ext = "png";
+                        break;
+                }
+                return string.Format("{0}-{1}.{2}", propertyName, owner.ID,ext);
+            }
+            set { }
+        }
+        public void LoadFromStream(string fileName, Stream stream) {
+            Guard.ArgumentNotNull(stream, "stream");
+            Guard.ArgumentNotNullOrEmpty(fileName, "fileName");
+            byte[] array = new byte[stream.Length];
+            stream.Read(array, 0, array.Length);
+            switch (propertyName) {
+                case PropertyNameEnum.Document:
+                    owner.Document = array;
+                    break;
+                case PropertyNameEnum.Screenshot:
+                    owner.Screenshot = array;
+                    break;
+            }
+        }
+        public void SaveToStream(Stream stream) {
+            if (string.IsNullOrEmpty(FileName)) {
+                throw new InvalidOperationException();
+            }
+            byte[] array = null;
+            switch (propertyName) {
+                case PropertyNameEnum.Document:
+                    array = owner.Document;
+                    break;
+                case PropertyNameEnum.Screenshot:
+                    array = owner.Screenshot;
+                    break;
+            }
+            if (array != null) {
+                stream.Write(array, 0, Size);
+            }
+            stream.Flush();
+        }
+        [Browsable(false)]
+        public int Size {
+            get {
+                byte[] array = null;
+                switch (propertyName) {
+                    case PropertyNameEnum.Document:
+                        array = owner.Document;
+                        break;
+                    case PropertyNameEnum.Screenshot:
+                        array = owner.Screenshot;
+                        break;
+                }
+                return array == null ? 0 : array.Length;
+            }
+        }
+        public override string ToString() {
+            return FileName;
+        }
+    }
+}
